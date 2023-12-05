64b5985b04d97c2f276b5e4be32c5ea36497a058
AznRaven <74807576+AznRaven@users.noreply.github.com>
2023-12-05 00:12:27 -0600

    escape error fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default function Home() {
             conveniently located in one unified platform.
           </h1>
           <p className="pb-20">
-            "Upgrade your personal storage experience with our Dropbox Clone,
+            &quot;Upgrade your personal storage experience with our Dropbox Clone,
             providing a streamlined and effective method to upload, organize,
             and access files from any location. Safely store crucial documents
             and media while enjoying the convenience of effortless file
             management and sharing—all within a single, secure, and centralized
-            solution."
+            solution.&quot;
           </p>
           <Link
             href="/dashboard"
@@ -42,10 +42,10 @@ export default function Home() {
       </div>
       <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
       <p className="text-center font-light p-2">
-        "This site is created solely for informational and educational purposes.
+        &quot;This site is created solely for informational and educational purposes.
         We are not affiliated with Dropbox or any of its subsidiaries in any
         way. Copyright Disclaimer under section 107 of the Copyright Act 1976
-        allows for 'fair use' of this site for educational purposes."
+        allows for &apos;fair use&apos; of this site for educational purposes.&quot;
       </p>
     </main>
   );
